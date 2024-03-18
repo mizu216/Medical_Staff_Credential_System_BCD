6791c9b1c0c1ab3d9fa3e966c9cada83e04db01e
Lim Zi Hong <limzihong216@gmail.com>
2024-03-19 01:25:26 +0800

    Initiate

---
+++ src/bcd_assignments/BCD_Assignments.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bcd_assignments;
+
+/**
+ *
+ * @author limzi
+ */
+public class BCD_Assignments {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
