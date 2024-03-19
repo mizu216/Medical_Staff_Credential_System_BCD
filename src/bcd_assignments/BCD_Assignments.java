f57a151d5444d3ec45fbe70350887c7b37292e80
TeeZeSheng <jasontzs007@gmail.com>
2024-03-19 13:00:37 +0800

    "added hello world"

--- src/bcd_assignments/BCD_Assignments.java
+++ src/bcd_assignments/BCD_Assignments.java
@@ -15,7 +15,11 @@ public class BCD_Assignments {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("hi, hi");
+        System.out.println("hi, hi");
+        System.out.println("hello world");
+        
+        
     }
     
 }
