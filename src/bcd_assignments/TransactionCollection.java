0b9057ad3cf8901dae75dc9c969783341f4753bb
TeeZeSheng <jasontzs007@gmail.com>
2024-03-29 12:36:54 +0800

    commit

---
+++ src/bcd_assignments/TransactionCollection.java
@@ -0,0 +1,40 @@
+package bloackchain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionCollection implements Serializable{
+	int SIZE = 10;
+	String merkleRoot;
+	List<String> tranxList = new ArrayList<>();
+	private static final long serialVersionUID = 1L;
+	
+	public void add(String tranx) {
+		tranxList.add(tranx);
+	}
+
+	@Override
+	public String toString() {
+		String res = "";
+		for(String credentials: tranxList) {
+			res = res + credentials + ",";
+		}
+		return res;
+	}
+	
+	public String getMerkleRoot() {
+		MerkleTree mt = MerkleTree.getInstance(tranxList);
+		mt.build();
+		String root = mt.getRoot();
+		return root;
+		
+	}
+	
+	
+
+	
+	
+	
+
+}
